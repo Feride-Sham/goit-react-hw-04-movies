3b21481f5f3b3fa378b487ac62bd111be471ab04
Feride Shamsutdinova <ferika.sham@gmail.com>
2021-03-18 14:20:59 +0300

    feat: add Switch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import HomePage from "./views/HomePage";
+import MoviesPage from "./views/MoviesPage";
+import NotFoundView from "./views/NotFoundView";
 
 import "./App.css";
 
 const App = () => {
   return (
     <>
-      <Route path="/" component={HomePage} />
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/movies">Movies</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/movies" component={MoviesPage} />
+        <Route component={NotFoundView} />
+      </Switch>
     </>
   );
 };
---
+++ src/views/MoviesPage.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const MoviesPage = () => {
+  return <h1>MoviesPage</h1>;
+};
+export default MoviesPage;
