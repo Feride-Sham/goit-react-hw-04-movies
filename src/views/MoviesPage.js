44fb370b4312a0fe58b9ccc272aab837ea7989ec
Feride Shamsutdinova <ferika.sham@gmail.com>
2021-03-18 20:25:21 +0300

    feat: add render gallery

--- src/views/MoviesPage.js
+++ src/views/MoviesPage.js
@@ -1,6 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
+// import axios from "axios";
 
-const MoviesPage = () => {
-  return <h1>MoviesPage</h1>;
-};
+// import movieAPI from "../services/movieAPI";
+
+// const baseURL = `https://api.themoviedb.org/3/`;
+// const myAPIkey = "2955876276611e1cc2d97a4794387b9d";
+
+class MoviesPage extends Component {
+  // state = {
+  //   movies: [],
+  // };
+
+  // async componentDidMount() {
+  //   const response = await axios.get(
+  //     `${baseURL}trending/all/day?api_key=${myAPIkey}`
+  //   );
+  //   const data = await response.data;
+  //   const moviesList = await data.results;
+  //   console.log(moviesList);
+  //   this.setState({ movies: moviesList });
+  // }
+
+  render() {
+    // const { movies } = this.state;
+    return (
+      <>
+        <h1>MoviesPage</h1>
+        {/* <ul>
+          {movies.map((movie) => {
+            return <li key={movie.id}>{movie.original_title}</li>;
+          })}
+        </ul> */}
+      </>
+    );
+  }
+}
+
 export default MoviesPage;
