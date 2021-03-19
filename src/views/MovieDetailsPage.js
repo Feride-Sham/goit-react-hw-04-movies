74bd3b1915139127775f312c4f1fbdd55af289c1
Feride Shamsutdinova <ferika.sham@gmail.com>
2021-03-19 15:40:36 +0300

    feat: add MovieDetailsPage structure

--- src/views/MovieDetailsPage.js
+++ src/views/MovieDetailsPage.js
@@ -5,6 +5,7 @@ class MovieDetailsPage extends Component {
   state = {
     imgDetail: {},
     imgBaseUrl: "https://image.tmdb.org/t/p/w500",
+    imgGenres: [],
   };
 
   async componentDidMount() {
@@ -13,23 +14,44 @@ class MovieDetailsPage extends Component {
     console.log(movieID);
     getDetailsMovie(movieID).then((result) => {
       console.log(result);
-      this.setState({ imgDetail: result });
+      this.setState({ imgDetail: result, imgGenres: result.genres });
     });
   }
 
   render() {
-    const { imgDetail, imgBaseUrl } = this.state;
+    const { imgDetail, imgBaseUrl, imgGenres } = this.state;
+    const {
+      poster_path,
+      original_title,
+      id,
+      vote_average,
+      overview,
+    } = imgDetail;
     return (
       <>
-        <h1>MovieDetailsPage {this.props.match.params.movieID}</h1>
-        <img
-          loading="lazy"
-          src={imgBaseUrl + imgDetail.poster_path}
-          alt={imgDetail.original_title}
-          data={imgDetail.id}
-          width="280"
-        />
-        <h2>{imgDetail.original_title}</h2>
+        <div>
+          <div>
+            <h1>MovieDetailsPage {this.props.match.params.movieID}</h1>
+            <img
+              loading="lazy"
+              src={imgBaseUrl + poster_path}
+              alt={original_title}
+              data={id}
+              width="280"
+            />
+          </div>
+          <div>
+            <h2>{original_title}</h2>
+            <p>User score: </p> <span>{vote_average}</span>
+            <p>Overview: </p> <p>{overview}</p>
+            <p>Genres:</p>{" "}
+            <ul>
+              {imgGenres.map((genre) => (
+                <li key={genre.id}>{genre.name}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </>
     );
   }
